491854d19d8ea9ccec543913bbfa54122ae077f9
dulmini11 <dulminihw2@gmail.com>
2025-06-07 23:20:37 +0530

    Create handle cell click funtion

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ updateGameMessage();
 resetButton.addEventListener("click", reset);
 cells.forEach((cell, index)=> cell.addEventListener("click",()=> handleCellClick(index)));
 
+function handleCellClick(index){
+    if(isGameFinished()|| cells [index].innerHTML !==""){
+        return
+    }
+
+gameStatus = EGameStatus.IN_PROGRESS;
+cells[index].innerHTML = currentPlayer;
+cells[index].classList.add("cell-"+ currentPlayer);
+
+if(checkWinner()){
+    gameStatus = EGameStatus.FINISHED_WITH_WINNER;
+    updateGameMessage();
+    return;
+}
+
+if (checkTie()){
+    gameStatus = EGameStatus.FINISHED_WITH_TIE;
+    updateGameMessage();
+}
+
+currentPlayer = currentPlayer === "X" ? "O" : "X";
+updateGameMessage();
+}
+
