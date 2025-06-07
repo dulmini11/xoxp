72a05394593885295a86ef642aaea1e536972c61
dulmini11 <dulminihw2@gmail.com>
2025-06-07 23:59:14 +0530

    Update js file

--- script.js
+++ script.js
@@ -1,90 +1,96 @@
 const EGameStatus = {
-    NOT_STARTED : "NOT_STARTED",
-    IN_PROGRESS : "IN_PROGRESS",
-    FINISHED_WITH_WINNER : "FINISHED_WITH_WINNER",
-    FINISHED_WITH_TIE : "FINISHED_WITH_TIE",
-};
+            NOT_STARTED : "NOT_STARTED",
+            IN_PROGRESS : "IN_PROGRESS",
+            FINISHED_WITH_WINNER : "FINISHED_WITH_WINNER",
+            FINISHED_WITH_TIE : "FINISHED_WITH_TIE",
+        };
 
-let gameStatus = EGameStatus.NOT_STARTED;
-let currentPlayer = "X"
-const cells = document.querySelectorAll(".cell");
-const resetButton = document.getElementById("reset-button");
-const gameMessage = document.querySelector(".game-message")
+        let gameStatus = EGameStatus.NOT_STARTED;
+        let currentPlayer = "X"
+        const cells = document.querySelectorAll(".cell");
+        const resetButton = document.getElementById("reset-button");
+        const gameMessage = document.querySelector(".game-message")
 
-updateGameMessage();
-resetButton.addEventListener("click", reset);
-cells.forEach((cell, index)=> cell.addEventListener("click",()=> handleCellClick(index)));
+        updateGameMessage();
+        resetButton.addEventListener("click", reset);
+        cells.forEach((cell, index)=> cell.addEventListener("click",()=> handleCellClick(index)));
 
-function handleCellClick(index){
-    if(isGameFinished()|| cells [index].innerHTML !==""){
-        return
-    }
+        function handleCellClick(index){
+            if(isGameFinished()|| cells [index].innerHTML !==""){
+                return
+            }
 
-gameStatus = EGameStatus.IN_PROGRESS;
-cells[index].innerHTML = currentPlayer;
-cells[index].classList.add("cell-"+ currentPlayer);
+        gameStatus = EGameStatus.IN_PROGRESS;
+        cells[index].innerHTML = currentPlayer;
+        cells[index].classList.add("cell-"+ currentPlayer);
 
-if(checkWinner()){
-    gameStatus = EGameStatus.FINISHED_WITH_WINNER;
-    updateGameMessage();
-    return;
-}
-
-if (checkTie()){
-    gameStatus = EGameStatus.FINISHED_WITH_TIE;
-    updateGameMessage();
-}
-
-currentPlayer = currentPlayer === "X" ? "O" : "X";
-updateGameMessage();
-}
-
-function checkWinner(){
-    const winPatterns = [
-        [0,1,2],[3,4,5],[6,7,8],//row
-        [0,3,6],[1,4,7],[2,5,8],//colume
-        [0,4,8],[2,4,6],//diagonals
-    ];
-
-    for (const pattern of winPatterns) {
-        const[a,b,c] = pattern;
-        if(
-            cells[a].innerHTML !== "" &&
-            cells[a].innerHTML === cells[b].innerHTML &&
-            cells[a].innerHTML=== cells[c].innerHTML 
-        ){
-            updateCellsWinners (cells[a], cells[b],cells[c]);
-            return true;
+        if(checkWinner()){
+            gameStatus = EGameStatus.FINISHED_WITH_WINNER;
+            updateGameMessage();
+            return;
         }
-    }
-    return false;
-}
 
-function checkTie() { 
-return Array.from(cells).every((cell)  => cell.innerHTML != ""); 
-} 
+        if (checkTie()){
+            gameStatus = EGameStatus.FINISHED_WITH_TIE;
+            updateGameMessage();
+        }
 
-function reset() {
-    gameStatus = EGameStatus.NOT_STARTED; 
-    currentPlayer = "X"; 
-    updateGameMessage(); 
-    cells.forEach( (cell) => { 
-    cell.innerText = ""; 
-    cell.className = "cell";
-    });
-    }
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        updateGameMessage();
+        }
 
-function updateGameMessage() { 
-  if (gameStatus === EGameStatus.FINISHED_WITH_TIE) { 
-    gameMessage.innerHTML = "It's a tie!"; 
-  } else if (gameStatus === EGameStatus.FINISHED_WITH_WINNER) { 
-    gameMessage.innerHTML = `Winner: <span>${currentPlayer}</span>`; 
-  } else { 
-    gameMessage.innerHTML = `Who's Turn: <span>${currentPlayer}</span>`; 
-  } 
-} 
+        function checkWinner(){
+            const winPatterns = [
+                [0,1,2],[3,4,5],[6,7,8],//row
+                [0,3,6],[1,4,7],[2,5,8],//colume
+                [0,4,8],[2,4,6],//diagonals
+            ];
 
-function isGameFinished() { 
-  return gameStatus === EGameStatus.FINISHED_WITH_TIE || 
-         gameStatus === EGameStatus.FINISHED_WITH_WINNER;
-}
+            for (const pattern of winPatterns) {
+                const[a,b,c] = pattern;
+                if(
+                    cells[a].innerHTML !== "" &&
+                    cells[a].innerHTML === cells[b].innerHTML &&
+                    cells[a].innerHTML=== cells[c].innerHTML 
+                ){
+                    updateCellsWinners (cells[a], cells[b],cells[c]);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function updateCellsWinners(cellA, cellB, cellC) {
+            cellA.classList.add("correct");
+            cellB.classList.add("correct");
+            cellC.classList.add("correct");
+        }
+
+        function checkTie() { 
+        return Array.from(cells).every((cell)  => cell.innerHTML != ""); 
+        } 
+
+        function reset() {
+            gameStatus = EGameStatus.NOT_STARTED; 
+            currentPlayer = "X"; 
+            updateGameMessage(); 
+            cells.forEach( (cell) => { 
+            cell.innerHTML = ""; 
+            cell.className = "cell";
+            });
+            }
+
+        function updateGameMessage() { 
+          if (gameStatus === EGameStatus.FINISHED_WITH_TIE) { 
+            gameMessage.innerHTML = "It's a tie!"; 
+          } else if (gameStatus === EGameStatus.FINISHED_WITH_WINNER) { 
+            gameMessage.innerHTML = `Winner: <span>${currentPlayer}</span>`; 
+          } else { 
+            gameMessage.innerHTML = `Who's Turn: <span>${currentPlayer}</span>`; 
+          } 
+        } 
+
+        function isGameFinished() { 
+          return gameStatus === EGameStatus.FINISHED_WITH_TIE || 
+                 gameStatus === EGameStatus.FINISHED_WITH_WINNER;
+        }
